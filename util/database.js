6851ef140decd7a0c098ee1cb1df0257c04669a5
Roger Chen <mengqiu.chen@hotmail.com>
2022-02-19 00:43:13 -0700

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,10 +1,17 @@
 import mongodb from "mongodb";
+import { readFile } from 'fs/promises';
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
+const json = JSON.parse(
+  await readFile(
+    new URL('../mongoDBAUth.json', import.meta.url)
+  )
+);
+
 export const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://<user>:<password>@cluster0.f6tfr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  MongoClient.connect(`mongodb+srv://${json.username}:${json.password}@cluster0.f6tfr.mongodb.net/Inventory?retryWrites=true&w=majority`)
   .then(client => {
     console.log("MongoDB connected !");
     _db = client.db();
